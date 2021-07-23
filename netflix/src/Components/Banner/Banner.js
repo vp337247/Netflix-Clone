6ad22b783c0241a66bf7d69c5b015e5dee7a5cd3
Vishnu Prasad <vishnu337247@gmail.com>
2021-07-23 14:34:38 +0530

    Banner set

--- netflix/src/Components/Banner/Banner.js
+++ netflix/src/Components/Banner/Banner.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import axios from '../../axios' //this is custom axios not installed one
 import './Banner.css'
+import { API_KEY,imageUrl } from '../../Constants/Constants'
+
+
 
 function Banner() {
+    const [movie, setMovie] = useState()
+    
+    
+    useEffect(() => {
+        axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+            console.log(response.data.results[0]) // for first data print
+            setMovie(response.data.results[ Math.floor(Math.random() * 21) ])
+        })
+    }, [])
     return (
-        <div className='banner'>
+        <div 
+        style={{backgroundImage: `url(${ movie ? imageUrl + movie.backdrop_path : ""})`}} className='banner'>
             <div className='content'>
-                <h1 className='title'>Movie Name</h1>
+                <h1 className='title'>{movie ? movie.title : ""}</h1>
                 <div className='banner_buttons'>
                     <button className='button'>Play</button>
                     <button className='button'>My list</button>
                 </div>
-                <h1 className='description'> publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the</h1>
+                <h1 className='description'> {movie ? movie.overview : "Description"}</h1>
 
             </div>
             <div className="fade_bottom"></div>
---
+++ netflix/src/Constants/Constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = "836bdf1d6e53dee1fe6e8adf4ba804ca";
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
